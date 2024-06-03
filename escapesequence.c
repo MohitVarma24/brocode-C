bb503f3bf6268c86fa643197dbb3149099c69646
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-03 20:22:11 +0530

    added escapestatus.c

---
+++ escapesequence.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    /*ESCAPE SEQUENCE = CHARACTER COMBINATION WHICH HAS A BACKSLASH(\)
+                       \n = NEWLINE
+                       \t = TAB*/
+    printf("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\t");
+    return 0;
+}
