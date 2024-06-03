e9650c3896e84318b206655f9e697b9a8c7fb422
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-03 20:23:14 +0530

    variables.c

---
+++ variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int age = 17; // INTEGER
+    float pi = 3.14; // FOR DECIMALS
+    char star = '*'; // FOR SINGLE CHAR
+    char name[]="musk";
+    printf("my name is %s\n", name);
+    printf("I am %d years old\n", age);
+    printf("The value of pi is %f\n", pi);
+    printf("This is a star : %c\n", star);
+    return 0;
+}
