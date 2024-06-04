aaa48e6921ba68c4ee78ef44a3ae56d189d5c4f4
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-04 19:27:29 +0530

    added arithmeticoperator.c

---
+++ arithmeticoperator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main (){
+    // ARITHMETIC OPERATORS
+    // + (addition)
+    // - (substraction)
+    // * (multiply)
+    // / (divide)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+    int x = 2;
+    int y = 4; 
+
+    int z = x + y;
+    int a = x - y;
+    int b = x * y;
+    int c = x / y;
+    int d = x % y;
+    printf("%d\n", z);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    return 0;
+}
