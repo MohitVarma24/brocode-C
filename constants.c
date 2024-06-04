638cbae0d3da900c646dd7a7532c2408c7c85f98
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-04 19:06:51 +0530

    added constants.c

---
+++ constants.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    // CONSTANTS = fixed value that cannot be altered by the program during execution
+    const float PI = 3.14159;
+    printf("%f", PI);
+    return 0;
+}
