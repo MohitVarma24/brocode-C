571adb1651edaa2b7d873cbb1f75eadfa43c7b4e
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-05 22:36:27 +0530

    added calculator.c

---
+++ calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("Enter an operator (+ - * /)");
+    scanf("%c", &operator);
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator){
+        case '+':
+        result = num1 + num2;
+        printf("Result: %lf", result);
+        break;
+        case '-':
+        result = num1 - num2;
+        printf("Result: %lf", result);
+        break;
+        case '*':
+        result = num1 * num2;
+        printf("Result: %lf", result);
+        break;
+        case '/':
+        result = num1 / num2;
+        printf("Result: %lf", result);
+        break;
+    }    
+    return 0;
+}
