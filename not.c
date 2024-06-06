97ede653d9ae942131ac5903acdcbf5eb42089c7
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-06 18:59:41 +0530

    added not.c

---
+++ not.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    bool sunny =0;
+    if (!sunny){
+    printf("It is sunny outside");
+    }
+    else{
+        printf("It is cloudy outside");
+    };
+return 0;
+}
