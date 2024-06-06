d0bced9f4997c3bad1ecd35a7c0e0c7728b77247
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-06 18:43:48 +0530

    added and.c

---
+++ and.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float temp = 50;
+    if (temp >= 0 && temp <= 40){
+    printf("The temperature is perfect!");
+    }
+    else{
+        printf("The temperature is BAD!");
+    };
+return 0;
+}
