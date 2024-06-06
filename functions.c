d6ddfe40beda8ed70eee036b7e2a8b99d6139604
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-06 20:07:44 +0530

    added functions.c

---
+++ functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void birthday(){
+    printf("\nHappy birthday to you\n");
+    printf("Happy birthday to you\n");
+    printf("Happy birthday to dear youu..\n");
+    printf("Happy birthday to you\n");
+}
+int main(){
+    birthday();
+    birthday();
+    birthday();
+    
+return 0;
+}
