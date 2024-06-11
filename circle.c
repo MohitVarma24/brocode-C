8c3a244d7fc1c1fe315606ffe1a75dd722f159bd
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-06-11 21:47:57 +0530

    added circle.c

--- circle.c
+++ circle.c
@@ -3,9 +3,11 @@ int main(){
     int radius;
     int circumference;
     int area;
-    printf("Enter the radius of the circle:");
+    printf("Enter the radius of the circle: ");
     scanf("%d", &radius);
     circumference = 2 * 3.14 * radius;
-    printf("The radius of the circle is: %d", circumference);
+    printf("The radius of the circle is: %d\nt", circumference);
+    area = 3.14 * radius * radius;
+    printf("The area of the circle is: %d", area);
     return 0;
 }
