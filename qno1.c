7386b0465ee536da462b490c5e6467391ad38a27
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-09-06 22:32:54 +0530

    added qno.c

---
+++ qno1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6;
+    int b = 9;
+    int add = a + b;
+    int sub = a - b;
+    int multiply = a * b;
+
+    printf("\n the sum of a & b is = %d", add);
+    printf("\n the sum of a & b is = %d", sub);
+    printf("\n the product of a & b is = %d", multiply);
+
+    return 0;
+}
